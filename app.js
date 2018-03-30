d0ff590a491f66782ce61c0374ecf3a7b3a4d0c4
Jim Shaw <jimxshaw@gmail.com>
2018-03-29 21:26:59 -0500

    Incorporate chalk in app.js.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var chalk = require("chalk");
 
 var app = express();
 
@@ -7,6 +8,6 @@ app.get("/", function (req, res) {
 });
 
 app.listen(3000, function () {
-  console.log("listening on port 3000");
+  console.log(`listening on port + ${chalk.greenBright("3000")}`);
 });
 
