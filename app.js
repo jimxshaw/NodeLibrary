5373b093ac0f4106dc15ed2d061dc5e0a25b1340
Jim Shaw <jimxshaw@gmail.com>
2018-03-31 17:37:27 -0500

    Remove redundant files and choose to use files from node modules instead.

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ var app = express();
 app.use(morgan("combined"));
 // The project has static directories to hold
 // static files. Specify a directory for those files.
+// If the files are not in the specified directory
+// then use the files from the node modules.
 app.use(express.static(path.join(__dirname, "/public")));
+app.use("/css", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js")));
+app.use("/js", express.static(path.join(__dirname, "/node_modules/jquery/dist")));
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "views/index.html"));
